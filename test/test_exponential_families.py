902c05911b8a18d1fcf0cdb8fc4dfd22bf4db925
Sean Robert Bittner <seanrbittner@gmail.com>
2020-03-23 11:40:02 -0400

    tests and docs for exponential families.

--- test/test_exponential_families.py
+++ test/test_exponential_families.py
@@ -1,10 +1,13 @@
 """ Test exponential families."""
 
 import numpy as np
+import torch
 from torch_nf.exponential_families import ExponentialFamily, MVN, Dirichlet
 from torch_nf.bijectors import ToSimplex
 from pytest import raises
 
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 def test_exponential_family_init():
     """Test ExponentialFamily class initialization."""
@@ -34,7 +37,12 @@ def test_exponential_family_init():
 
 
 def test_MVN():
+    def eye_var(D, Sigma):
+        eye_ref = np.expand_dims(np.eye(D), 0)
+        return np.sum((Sigma - eye_ref)**2)
+
     Ds = [2, 5, 20]
+    M = 20
     N = 50
     for D in Ds:
         ef = MVN(D)
@@ -42,7 +50,7 @@ def test_MVN():
         assert ef.D_eta == (D + (D * (D + 1) // 2))
         assert ef.support_layer is None
 
-        eta = ef.sample_eta(N)
+        eta = ef.sample_eta(N, sigma_mu=1., iw_df_fac=5)
         assert eta.shape[0] == N
         assert eta.shape[1] == ef.D_eta
 
@@ -53,13 +61,44 @@ def test_MVN():
         assert Sigma.shape[1] == D
         assert Sigma.shape[2] == D
 
+        # Check forward and backward mappings for EF are inverses.
         _eta = ef.mu_to_eta(mu, Sigma)
         assert np.isclose(eta, _eta).all()
+       
+        # Check properties of p(eta).
+        mu_var = np.var(mu)
+        Sigma_eye_var = eye_var(D, Sigma)
+        for i in range(10):
+            eta2 = ef.sample_eta(N, sigma_mu=2., iw_df_fac=20)
+            mu2, Sigma2 = ef.eta_to_mu(eta2)
+
+            # Make sure sigma_mu controls variance of sampled mus.
+            mu2_var = np.var(mu2)
+            assert(mu_var < mu2_var)
+            # Make sure iw_df_fac controls the adherance to I for Sigma.
+            Sigma2_eye_var = eye_var(D, Sigma2)
+            assert(Sigma_eye_var > Sigma2_eye_var)
+
+        # Check output of T.
+        z = torch.tensor(np.random.normal(0., 10., (M, N, D)))
+        T_z = ef.T(z)
+
+        T_z_true = np.zeros((M,N,ef.D_eta))
+        T_z_true[:,:,:D] = z
+        zzT = np.matmul(z[:, :, :, None], z[:, :, None, :])
+        ind = D
+        for i in range(D):
+            for j in range(i,D):
+                T_z_true[:,:,ind] = zzT[:,:,i,j]
+                ind += 1
+        assert(np.isclose(T_z.numpy(), T_z_true).all())
+
     return None
 
 
 def test_Dirichlet():
     Ds = [2, 5, 20]
+    M = 20
     N = 50
     for D in Ds:
         ef = Dirichlet(D)
@@ -75,13 +114,38 @@ def test_Dirichlet():
         assert alpha.shape[0] == N
         assert alpha.shape[1] == D
 
+        # Check forward and backward mappings for EF are inverses.
         _eta = ef.mu_to_eta(alpha)
         assert np.isclose(eta, _eta).all()
 
+        # Check properties of p(eta).
+        alpha_var = np.var(alpha)
+        alpha_min = np.min(alpha)
+        alpha_max = np.max(alpha)
+        for i in range(10):
+            eta2 = ef.sample_eta(N, lb=0.1, ub=3.)
+            alpha2 = ef.eta_to_mu(eta2)
+
+            # Make sure bounds are modifying distribution.
+            alpha2_var = np.var(alpha2)
+            assert(alpha_var < alpha2_var)
+            alpha2_min = np.min(alpha2)
+            assert(alpha2_min < alpha_min)
+            alpha2_max = np.max(alpha2)
+            assert(alpha2_max > alpha_max)
+
+        # Check output of T.
+        z = torch.tensor(np.random.uniform(0.1, 3., (M, N, D)))
+        T_z = ef.T(z)
+
+        _T_z = np.log(z.numpy())
+        log_h_z = np.sum(_T_z, axis=2)
+        T_z_true = np.concatenate((_T_z, np.expand_dims(log_h_z, 2)), axis=2)
+        assert(np.isclose(T_z.numpy(), T_z_true).all())
     return None
 
 
 if __name__ == "__main__":
-    test_exponential_family_init()
-    test_MVN()
+    #test_exponential_family_init()
+    #test_MVN()
     test_Dirichlet()
--- torch_nf/exponential_families.py
+++ torch_nf/exponential_families.py
@@ -1,3 +1,5 @@
+""" Exponential family classes for implementing EFN. """
+
 import torch
 import numpy as np
 import scipy
@@ -6,6 +8,17 @@ from torch_nf.bijectors import ToSimplex
 
 
 class ExponentialFamily(object):
+    """ Exponential family distributions for training EFNs.
+
+    In this class, eta is the natural parameter and T calculates the sufficient
+    statistics.  Here, eta is the augmented natural parameter that has a 1
+    tacked onto the end when the log base measure is not a constant w.r.t. z.
+    Likewise, T tacks on log(h(z)) when it is not a constant for the 
+    exponential family.
+
+    :param D: Dimensionality of random variable.
+    :type D: int
+    """
     def __init__(self, D):
         super().__init__()
         self.D = D
@@ -13,64 +26,54 @@ class ExponentialFamily(object):
         self.D_eta = self._get_D_eta()
 
     def _get_D_eta(self,):
+        """Calculate dimensionality of natural parameter.
+
+        :return: Dimensionality of natural parameter eta.
+        :rtype: int
+        """
         return self.D
 
     def sample_eta(self, N):
+        """Sample from prior distribution of the natural parameter eta.
+
+        :param N: Number of eta samples.
+        :type N: int
+        :return: Samples of eta (N, D_eta).
+        :rtype: np.ndarray
+        """
         raise NotImplementedError()
 
     def mu_to_eta(self, mu):
+        """Obtain the natural parameters (eta) from the mean parameterization (mu).
+
+        :param mu: Mean parameterization (N, D_mu).
+        :type mu: np.ndarray
+        :return: eta (N, D_eta).
+        :rtype: np.ndarray
+        """
         raise NotImplementedError()
 
     def eta_to_mu(self, eta):
+        """Obtain the mean parameterization (mu) from the natural parameters (eta).
+
+        :param eta: Mean parameterization (N, D_eta).
+        :type eta: np.ndarray
+        :return: mu (N, D_mu).
+        :rtype: np.ndarray
+        """
         raise NotImplementedError()
 
     def T(self, z):
+        """Sufficient statistic of the exponential family.
+
+        :param z: Random variable of the exponential family (M, N, D).
+        :type z: torch.tensor
+        :return: T(z) (M, N, D_eta).
+        :rtype: torch.tensor
+        """
         raise NotImplementedError()
 
 
-class Dirichlet(ExponentialFamily):
-    def __init__(self, D):
-        super().__init__(D)
-        self.support_layer = ToSimplex
-
-    def _get_D_eta(self,):
-        return self.D + 1
-
-    def sample_eta(self, N=50):
-        alpha = np.random.uniform(0.5, 2.0, (N, self.D))
-        # need to tack on 1's for the base measure
-        eta = np.concatenate((alpha, np.ones((N, 1))), axis=1)
-        return eta
-
-    def T(self, z):
-        # The suff stats
-        log_z = torch.log(z)
-        # Need to tack on the base measure as well
-        h_z = torch.sum(log_z, dim=2, keepdim=True)
-        return torch.cat((log_z, h_z), axis=2)
-
-    def mu_to_eta(self, alpha):
-        N = alpha.shape[0]
-        eta = np.concatenate((alpha, np.ones((N, 1))), axis=1)
-        return eta
-
-    def eta_to_mu(self, eta):
-        alpha = eta[:, : self.D]
-        return alpha
-
-    def KL(self, z, log_prob, eta):
-        M = z.shape[0]
-        N = z.shape[1]
-        KLs = np.zeros((M,))
-        alpha = self.eta_to_mu(eta)
-        simplex_eps = 1e-32
-        for i in range(M):
-            dist = scipy.stats.dirichlet(alpha=np.float64(alpha[i]))
-            zi = np.float64(z[i]) + simplex_eps
-            zi = zi / np.expand_dims(np.sum(zi, 1), 1)
-            log_p_z = dist.logpdf(zi.T)
-            KLs[i] = np.mean(log_prob[i] - log_p_z)
-        return KLs
 
 
 class MVN(ExponentialFamily):
@@ -78,12 +81,30 @@ class MVN(ExponentialFamily):
         super().__init__(D)
 
     def _get_D_eta(self,):
+        """Calculate dimensionality of natural parameter.
+
+        :return: Dimensionality of natural parameter eta.
+        :rtype: int
+        """
         return int(self.D + (self.D * (self.D + 1) // 2))
 
-    def sample_eta(self, N=50):
-        df_fac = 5
-        mu = np.random.normal(0.0, 1.0, (N, self.D))
-        df = df_fac * self.D
+    def sample_eta(self, N=50, sigma_mu=1., iw_df_fac=5):
+        """Sample from prior distribution of the natural parameter eta.
+
+        :math:`\\mu_i \\sim \\mathcal{N}(0, \\sigma_{\\mu})`
+        :math:`\\Sigma \\sim \\mathcal{IW}(df=iw_df_fac*D, scale=df_fac*D*I)`
+
+        :param N: Number of eta samples.
+        :type N: int, optional
+        :param sigma_mu: Standard deviation of Gaussian prior on :math:`\\mu`.
+        :type sigma_mu: float, optional
+        :param iw_df_fac: Degree of freedom multiplier.  df=iw_df_fac*D.
+        :type iw_df_fac: int, optional
+        :return: Samples of eta (N, D_eta).
+        :rtype: np.ndarray
+        """
+        mu = np.random.normal(0.0, sigma_mu, (N, self.D))
+        df = iw_df_fac * self.D
         iw = invwishart(df=df, scale=df * np.eye(self.D))
         Sigma = iw.rvs(N)
         if N == 1:
@@ -91,20 +112,45 @@ class MVN(ExponentialFamily):
         return self.mu_to_eta(mu, Sigma)
 
     def T(self, z):
+        """Sufficient statistic of the exponential family.
+
+        :math:`T(z) = z, zz^\top`
+        
+        We eliminate redundancy in T(z) by vectorizing the upper triangular
+        part of the second moment matrix.
+
+        :param z: Random variable of the exponential family (M, N, D).
+        :type z: torch.tensor
+        :return: T(z) (M, N, D_eta).
+        :rtype: torch.tensor
+        """
         utri0_inds = np.triu_indices(self.D, 0)
         zzT = torch.matmul(z[:, :, :, None], z[:, :, None, :])
         zzT = zzT[:, :, utri0_inds[0], utri0_inds[1]]
         return torch.cat((z, zzT), axis=2)
 
     def mu_to_eta(self, mu, Sigma):
+        """Obtain the natural parameters (eta) from the mean parameterization (mu, Sigma).
+
+        To avoid redundancy, we eliminate the copy of symmetric elements of the
+        second moment natural parameter.  We multiply such off diagonal elems
+        by two in their vectorization in eta.
+
+        :param mu: Mean (N, D)
+        :type mu: np.ndarray
+        :param Sigma: Covariance (N, D, D).
+        :type Sigma: np.ndarray
+        :return: eta (N, D_eta).
+        :rtype: np.ndarray
+        """
         utri0_inds = np.triu_indices(self.D, 0)
         utri1_inds = np.triu_indices(self.D, 1)
 
         Sigma_inv = np.linalg.inv(Sigma)
         eta1 = np.float64(np.matmul(Sigma_inv, np.expand_dims(mu, 2)))
         eta2 = np.float64(-Sigma_inv / 2)
-        # by using the minimal representation, we need to multiply eta by two
-        # for the off diagonal elements
+        # Since we're using the minimal representation, we need to multiply eta
+        # by two for the off diagonal elements.
         eta2[:, utri1_inds[0], utri1_inds[1]] = (
             2 * eta2[:, utri1_inds[0], utri1_inds[1]]
         )
@@ -113,6 +159,13 @@ class MVN(ExponentialFamily):
         return eta
 
     def eta_to_mu(self, eta):
+        """Obtain the mean parameterization (mu, Sigma) from the natural parameters (eta).
+
+        :param eta: Mean parameterization (N, D_eta).
+        :type eta: np.ndarray
+        :return: mu (N, D), Sigma (N, D, D).
+        :rtype: np.ndarray, np.ndarray
+        """
         N = eta.shape[0]
         eta1 = eta[:, : self.D]
         _eta2 = eta[:, self.D :]
@@ -134,3 +187,95 @@ class MVN(ExponentialFamily):
             log_p_z = dist.logpdf(z[i])
             KLs[i] = np.mean(log_prob[i] - log_p_z)
         return KLs
+
+
+class Dirichlet(ExponentialFamily):
+    def __init__(self, D):
+        super().__init__(D)
+        self.support_layer = ToSimplex
+
+    def _get_D_eta(self,):
+        """Calculate dimensionality of natural parameter.
+
+        :return: Dimensionality of natural parameter eta.
+        :rtype: int
+        """
+        # We add one to the dimensionality of alpha for the log base measure.
+        return self.D + 1
+
+    def sample_eta(self, N=50, lb=0.5, ub=2.):
+        """Sample from prior distribution of the natural parameter eta.
+
+        :math:`\\alpha_i \\sim \\mathcal{U}U[lb, ub]`
+
+        A one is tacked onto the end of this natural parameter because there
+        is a non-constant log base measure for the Dirichlet.
+
+        :param N: Number of eta samples.
+        :type N: int, optional
+        :param lb: Lower bound of prior.
+        :type lb: float, optional
+        :param ub: Upper bound of prior.
+        :type ub: float, optional
+        :return: Samples of eta (N, D_eta).
+        :rtype: np.ndarray
+        """
+        alpha = np.random.uniform(lb, ub, (N, self.D))
+        # need to tack on 1's for the base measure
+        eta = np.concatenate((alpha, np.ones((N, 1))), axis=1)
+        return eta
+
+    def T(self, z):
+        """Sufficient statistic of the exponential family.
+
+        :math:`T(z) = \\log(z)
+        :math:`\\log(h(z)) = \\sum_i \\log(z_i)
+
+        The log base measure is concatenated onto the end of T(z).
+
+        :param z: Random variable of the exponential family (M, N, D).
+        :type z: torch.tensor
+        :return: T(z) (M, N, D_eta).
+        :rtype: torch.tensor
+        """
+        log_z = torch.log(z)
+        h_z = torch.sum(log_z, dim=2, keepdim=True)
+        return torch.cat((log_z, h_z), axis=2)
+
+    def mu_to_eta(self, alpha):
+        """Obtain the natural parameters (eta) from the mean parameterization (alpha).
+
+        :param alpha: Mean parameterization (N, D_mu).
+        :type alpha: np.ndarray
+        :return: eta (N, D_eta).
+        :rtype: np.ndarray
+        """
+        N = alpha.shape[0]
+        eta = np.concatenate((alpha, np.ones((N, 1))), axis=1)
+        return eta
+
+    def eta_to_mu(self, eta):
+        """Obtain the mean parameterization (alpha) from the natural parameters (eta).
+
+        :param eta: Mean parameterization (N, D_eta).
+        :type eta: np.ndarray
+        :return: mu (N, D_mu).
+        :rtype: np.ndarray
+        """
+        alpha = eta[:, : self.D]
+        return alpha
+
+    def KL(self, z, log_prob, eta):
+        M = z.shape[0]
+        N = z.shape[1]
+        KLs = np.zeros((M,))
+        alpha = self.eta_to_mu(eta)
+        simplex_eps = 1e-32
+        for i in range(M):
+            dist = scipy.stats.dirichlet(alpha=np.float64(alpha[i]))
+            zi = np.float64(z[i]) + simplex_eps
+            zi = zi / np.expand_dims(np.sum(zi, 1), 1)
+            log_p_z = dist.logpdf(zi.T)
+            KLs[i] = np.mean(log_prob[i] - log_p_z)
+        return KLs
+
