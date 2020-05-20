8cb2f71b8a6d1833683756ee8897344a406ca2cd
Sean Bittner <seanrbittner@gmail>
2020-05-20 12:40:42 -0400

    DensityEstimator base class

---
+++ tests/test_density_estimators.py
@@ -0,0 +1,158 @@
+""" Test conditional normalizing flows."""
+
+import numpy as np
+import torch
+#from torch_nf.conditional_nf import NormFlow, ConditionedNormFlow
+import torch_nf.bijectors as tnfb #Bijector, RealNVP, MAF, BatchNorm, ToSimplex, Affine
+import torch_nf.density_estimator as de
+
+from pytest import raises
+
+import os
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
+
+
+def test_DensityEstimator():
+    """Test DensityEstimator."""
+
+    D = 4
+    N = 100
+    M = 10
+    num_params = 8
+
+    conditioner = False
+    de1 = de.DensityEstimator(D, conditioner)
+    assert de1.D == D
+    assert not de1.conditioner
+
+    conditioner = True
+    de2 = de.DensityEstimator(D, conditioner)
+    assert de2.D == D
+    assert de2.conditioner
+
+    z = np.random.normal((M, N, D))
+    params = np.random.normal((M, num_params))
+    with raises(NotImplementedError):
+        de2(N, params)
+
+    with raises(NotImplementedError):
+        de2.log_prob(z, params)
+
+    with raises(NotImplementedError):
+        de2.count_num_params()
+
+    return None
+
+def test_MoG():
+    """Test NormFlow class initialization."""
+
+    D = 4
+    conditioner = False
+    K = 1
+    mog = de.MoG(D, conditioner, K)
+    assert mog.D == D
+    assert not conditioner
+    assert mog.K == K
+    return None
+
+
+def test_NormFlow():
+    """Test NormFlow class initialization."""
+
+    D = 4
+    arch_type = "coupling"
+    conditioner = False
+    num_stages = 1
+    num_layers = 2
+    num_units = 30
+    support_layer = None
+    nf = de.NormFlow(
+        D, conditioner, arch_type, num_stages, num_layers, num_units, support_layer
+    )
+    assert nf.arch_type == arch_type
+    assert nf.num_stages == num_stages
+    assert nf.num_layers == num_layers
+    assert nf.num_units == num_units
+    assert nf.support_layer == support_layer
+
+    nf = de.NormFlow(
+        D, conditioner, arch_type, num_stages, num_layers, 10, tnfb.ToSimplex(D)
+    )
+
+    assert nf.num_units == 15
+    assert issubclass(type(nf.support_layer), tnfb.Bijector)
+
+    with raises(TypeError):
+        nf = de.NormFlow('foo', False, 'coupling', 1, 2, 20, None)
+    with raises(ValueError):
+        nf = de.NormFlow(-1, False, 'coupling', 1, 2, 20, None)
+
+    with raises(TypeError):
+        nf = de.NormFlow(4, False, 1, 1, 2, 20, None)
+    with raises(ValueError):
+        nf = de.NormFlow(4, False, 'foo', 1, 2, 20, None)
+
+    with raises(TypeError):
+        nf = de.NormFlow(4, 1, 'coupling', 1, 2, 20, None)
+
+    with raises(TypeError):
+        nf = de.NormFlow(4, False, 'coupling', 'foo', 2, 20, None)
+    with raises(ValueError):
+        nf = de.NormFlow(4, False, 'coupling', -1, 2, 20, None)
+
+    with raises(TypeError):
+        nf = de.NormFlow(4, False, 'coupling', 1, 'foo', 20, None)
+    with raises(ValueError):
+        nf = de.NormFlow(4, False, 'coupling', 1, -1, 20, None)
+
+    with raises(TypeError):
+        nf = de.NormFlow(4, False, 'coupling', 1, 2, 'foo', None)
+    with raises(ValueError):
+        nf = de.NormFlow(4, False, 'coupling', 1, 2, -1, None)
+
+    with raises(TypeError):
+        nf = de.NormFlow(4, False, 'coupling', 1, 2, 20, 'foo')
+
+    nf = de.NormFlow(D, False, 'coupling', 1, 2, 20, None)
+
+    N = 10
+    z, log_q_z = nf(N)
+    assert(z.shape[0] == 1 and z.shape[1] == N and z.shape[2] == D)
+    assert(log_q_z.shape[0] == 1 and log_q_z.shape[1] == N)
+    log_q_z_inv = nf.log_prob(z)
+    assert(np.sum(np.square(log_q_z.detach().numpy() - log_q_z_inv.detach().numpy())) < 1e-2)
+
+    nf = de.NormFlow(D, True, 'coupling', 2, 2, 20, tnfb.ToSimplex(D))
+    assert(issubclass(type(nf.bijectors[0]), tnfb.RealNVP))
+    assert(issubclass(type(nf.bijectors[1]), tnfb.BatchNorm))
+    assert(issubclass(type(nf.bijectors[2]), tnfb.RealNVP))
+    assert(issubclass(type(nf.bijectors[3]), tnfb.BatchNorm))
+    assert(issubclass(type(nf.bijectors[4]), tnfb.Affine))
+    assert(issubclass(type(nf.bijectors[5]), tnfb.RealNVP))
+    assert(issubclass(type(nf.bijectors[6]), tnfb.BatchNorm))
+    assert(issubclass(type(nf.bijectors[7]), tnfb.RealNVP))
+    assert(issubclass(type(nf.bijectors[8]), tnfb.BatchNorm))
+    assert(issubclass(type(nf.bijectors[9]), tnfb.Affine))
+    assert(issubclass(type(nf.bijectors[10]), tnfb.ToSimplex))
+
+    nf = de.NormFlow(D, False, 'coupling', 2, 2, 20)
+    assert(issubclass(type(nf.bijectors[0]), tnfb.RealNVP))
+    z, log_q_z = nf(N)
+    log_q_z_inv = nf.log_prob(z)
+    assert(np.sum(np.square(log_q_z.detach().numpy() - log_q_z_inv.detach().numpy())) < 1e-2)
+
+    nf = de.NormFlow(D, False, 'AR', num_layers=2, num_units=20)
+    assert(issubclass(type(nf.bijectors[0]), tnfb.MAF))
+    z, log_q_z = nf(N)
+    log_q_z_inv = nf.log_prob(z)
+
+    assert(np.sum(np.square(log_q_z.detach().numpy() - log_q_z_inv.detach().numpy())) < 1e-2)
+
+    return None
+    
+
+if __name__ == "__main__":
+    test_DensityEstimator()
+    test_NormFlow()
+    test_MoG()
--- torch_nf/conditional_nf.py
+++ torch_nf/conditional_nf.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn.functional as F
 import numpy as np
+import torch_nf.density_estimator as de
 from torch_nf.error_formatters import format_type_err_msg
 from torch_nf.bijectors import RealNVP, MAF, BatchNorm, Affine, Bijector
 from collections import OrderedDict
@@ -42,8 +43,8 @@ class ConditionedNormFlow(torch.nn.Module):
 
     @nf.setter
     def nf(self, val):
-        if type(val) is not NormFlow:
-            raise TypeError(format_type_err_msg(self, "nf", val, NormFlow))
+        if type(val) is not de.NormFlow:
+            raise TypeError(format_type_err_msg(self, "nf", val, de.NormFlow))
         self.__nf = val
 
     @property
@@ -96,212 +97,6 @@ class ConditionedNormFlow(torch.nn.Module):
         params = self.param_net(x)
         log_prob = self.nf.log_prob(z, params)
         return log_prob
-
-
-class NormFlow(object):
-    def __init__(
-        self,
-        D,
-        arch_type,
-        conditioner=False,
-        num_stages=1,
-        num_layers=2,
-        num_units=15,
-        support_layer=None,
-    ):
-        super().__init__()
-        self.D = D
-        self.arch_type = arch_type
-        self.conditioner = conditioner
-        self.num_stages = num_stages
-        self.num_layers = num_layers
-        self.num_units = num_units
-        self.support_layer = support_layer
-
-        self.bijectors = []
-
-        if arch_type == "coupling":
-            for i in range(num_stages):
-                self.bijectors.append(
-                    RealNVP(D, num_layers, num_units, transform_upper=True)
-                )
-                self.bijectors.append(BatchNorm(D))
-                self.bijectors.append(
-                    RealNVP(D, num_layers, num_units, transform_upper=False)
-                )
-                self.bijectors.append(BatchNorm(D))
-                self.bijectors.append(Affine(D))
-        elif arch_type == "autoregressive":
-            self.bijectors.append(MAF(D, self.num_layers, self.num_units, fwd_fac=True))
-            self.bijectors.append(BatchNorm(D))
-            self.bijectors.append(Affine(D))
-        elif arch_type == "affine":
-            self.bijectors.append(Affine(D))
-
-        if support_layer is not None:
-            if issubclass(type(support_layer), Bijector):
-                self.bijectors.append(support_layer)
-            else:
-                raise TypeError("Support layer not Bijector.")
-
-        self.count_num_params()
-
-        if not self.conditioner:
-            self.params = torch.nn.init.xavier_normal_(
-                torch.zeros(1, self.D_params, requires_grad=True)
-            )
-
-    @property
-    def D(self,):
-        return self.__D
-
-    @D.setter
-    def D(self, val):
-        if type(val) is not int:
-            raise TypeError(format_type_err_msg(self, "D", val, int))
-        elif val < 2:
-            raise ValueError("NormalizingFlow D %d must be greater than 0." % val)
-        self.__D = val
-
-    @property
-    def arch_type(self,):
-        return self.__arch_type
-
-    @arch_type.setter
-    def arch_type(self, val):
-        arch_types = ["coupling", "autoregressive", "affine"]
-        if type(val) is not str:
-            raise TypeError(format_type_err_msg(self, "arch_type", val, str))
-        if val not in arch_types:
-            raise ValueError(
-                'NormalizingFlow arch_type must be "coupling", "autoregressive", or "affine".'
-            )
-        self.__arch_type = val
-
-    @property
-    def conditioner(self,):
-        return self.__conditioner
-
-    @conditioner.setter
-    def conditioner(self, val):
-        if type(val) is not bool:
-            raise TypeError(format_type_err_msg(self, "conditioner", val, bool))
-        self.__conditioner = val
-
-    @property
-    def num_stages(self,):
-        return self.__num_stages
-
-    @num_stages.setter
-    def num_stages(self, val):
-        if type(val) is not int:
-            raise TypeError(format_type_err_msg(self, "num_stages", val, int))
-        elif val < 1:
-            raise ValueError(
-                "NormalizingFlow num_stages %d must be greater than 0." % val
-            )
-        self.__num_stages = val
-
-    @property
-    def num_layers(self,):
-        return self.__num_layers
-
-    @num_layers.setter
-    def num_layers(self, val):
-        if type(val) is not int:
-            raise TypeError(format_type_err_msg(self, "num_layers", val, int))
-        elif val < 1:
-            raise ValueError(
-                "NormalizingFlow num_layers arg %d must be greater than 0." % val
-            )
-        self.__num_layers = val
-
-    @property
-    def num_units(self,):
-        return self.__num_units
-
-    @num_units.setter
-    def num_units(self, val):
-        if type(val) is not int:
-            raise TypeError(format_type_err_msg(self, "num_units", val, int))
-        elif val < 1:
-            raise ValueError(
-                "NormalizingFlow num_units %d must be greater than 0." % val
-            )
-        elif val < 15:
-            print(
-                "Warning: NormFlow.num_layers set to minimum of 15 (received %d)." % val
-            )
-            self.__num_units = 15
-        else:
-            self.__num_units = val
-
-    def __call__(self, N=100, params=None, freeze_bn=False):
-        if not self.conditioner:
-            return self.forward(self.params, N, freeze_bn=freeze_bn)
-        else:
-            return self.forward(params, N, freeze_bn=freeze_bn)
-
-    def forward(self, params, N=100, freeze_bn=False):
-        M = params.size(0)
-        omega = np.random.normal(0.0, 1.0, (M, N, self.D))
-        z = torch.tensor(omega).float()
-
-        log_q_z = np.log(
-            np.prod(np.exp((-np.square(omega)) / 2.0) / np.sqrt(2.0 * np.pi), axis=2)
-        )
-        log_q_z = torch.tensor(log_q_z)
-
-        idx = 0  # parameter index
-        for i, bijector in enumerate(self.bijectors):
-            if bijector.name == "BatchNorm":
-                z, log_det = bijector(z, use_last=freeze_bn)
-            else:
-                num_ps = (
-                    bijector.count_num_params()
-                )  # number of parameters for bijector
-                if num_ps > 0:
-                    z, log_det = bijector(z, params[:, idx : (idx + num_ps)])
-                    idx += num_ps
-                else:
-                    z, log_det = bijector(z)
-            log_q_z = log_q_z - log_det
-        return z, log_q_z
-
-    def inverse_and_log_det(self, z, params):
-        num_bijectors = len(self.bijectors)
-        z_size = z.size()
-        idx = self.D_params
-        sum_log_det = torch.zeros((z_size[0], z_size[1]))
-        for i in range(num_bijectors - 1, -1, -1):
-            bijector = self.bijectors[i]
-            num_ps = bijector.count_num_params()
-            if num_ps > 0:
-                z, log_det = bijector.inverse_and_log_det(
-                    z, params[:, (idx - num_ps) : idx]
-                )
-                idx -= num_ps
-            else:
-                z, log_det = bijector.inverse_and_log_det(z)
-            sum_log_det += log_det
-        return z, sum_log_det
-
-    def log_prob(self, z, params=None):
-        if not self.conditioner:
-            z, sum_log_det = self.inverse_and_log_det(z, self.params)
-        else:
-            z, sum_log_det = self.inverse_and_log_det(z, params)
-        log_q_z = torch.sum(-(z ** 2), axis=2) / 2.0 - self.D * np.log(
-            np.sqrt(2.0 * np.pi)
-        )
-        #log_q_z = torch.clamp(log_q_z, -1e10, 1e10)
-        return log_q_z - sum_log_det
-
-    def count_num_params(self,):
-        self.D_params = 0
-        for bijector in self.bijectors:
-            self.D_params += bijector.count_num_params()
-
 
 def dbg_check(tensor, name):
     num_elems = 1
