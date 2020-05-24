23cfa034b181858297d401881d77199657ba4655
Sean Bittner <seanrbittner@gmail>
2020-05-24 13:57:03 -0400

    more MoG tests

--- tests/test_density_estimators.py
+++ tests/test_density_estimators.py
@@ -2,8 +2,7 @@
 
 import numpy as np
 import torch
-#from torch_nf.conditional_nf import NormFlow, ConditionedNormFlow
-import torch_nf.bijectors as tnfb #Bijector, RealNVP, MAF, BatchNorm, ToSimplex, Affine
+import torch_nf.bijectors as bij 
 import torch_nf.density_estimator as de
 
 from pytest import raises
@@ -44,6 +43,29 @@ def test_DensityEstimator():
 
     return None
 
+def get_MoG_params(params, K, D):
+    beg_alpha = 0
+    beg_mu = beg_alpha+K
+    beg_Sigma_inv = beg_mu+(K*D)
+
+    _alpha = params[beg_alpha:beg_mu]
+    mu = np.reshape(params[beg_mu:beg_Sigma_inv], (K, D))
+    _U = np.reshape(params[beg_Sigma_inv:], (K, D*(D+1)//2))
+
+    # Softmax
+    exp_alpha = np.exp(_alpha)
+    alpha = exp_alpha / np.sum(exp_alpha)
+
+    inds = np.triu_indices(D)
+    U = np.zeros((K,D,D))
+    for k in range(K):
+        U[k,inds[0], inds[1]] = _U[k,:]
+    U[:,range(D),range(D)] = np.exp(U[:,range(D),range(D)])
+    Sigma_inv = np.matmul(np.transpose(U, (0,2,1)), U)
+
+    return alpha, mu, Sigma_inv
+    
+
 def test_MoG():
     """Test NormFlow class initialization."""
 
@@ -63,16 +85,42 @@ def test_MoG():
     mog.count_num_params()
 
     params = torch.normal(0., 1., (M, mog.D_params))
-    alpha, mu, Sigma = mog._get_MoG_params(params)
+    alpha, mu, Sigma_inv, Sigma_det = mog._get_MoG_params(params)
 
+    # Test that alpha obeys softmax property.
     assert np.isclose(alpha.sum(1).numpy(), 1.).all()
+    # Test that Sigma_mk is PSD.
     for m in range(M):
         for k in range(K):
-            Sigma_mk = Sigma[m,k,:,:]
-            assert (Sigma_mk[range(D),range(D)] >= 0.).all()
-            assert np.isclose(Sigma_mk, Sigma_mk.T).all()
+            Sigma_inv_mk = Sigma_inv[m,k,:,:]
+            assert (Sigma_inv_mk[range(D),range(D)] >= 0.).all()
+            assert np.isclose(Sigma_inv_mk, Sigma_inv_mk.T).all()
 
-    z = mog.forward(params, N=10)
+    alpha_true, mu_true, Sigma_inv_true, Sigma_det_true = [], [], [], []
+    for i in range(M):
+        _alpha, _mu, _Sigma_inv = get_MoG_params(params[i,:].numpy(), K, D)
+        alpha_true.append(_alpha)
+        mu_true.append(_mu)
+        Sigma_inv_true.append(_Sigma_inv)
+        Sigma_det_true.append(1. / np.linalg.det(_Sigma_inv))
+    alpha_true = np.array(alpha_true)
+    mu_true = np.array(mu_true)
+    Sigma_inv_true = np.array(Sigma_inv_true)
+    Sigma_det_true = np.array(Sigma_det_true)
+
+    print(alpha_true.shape, alpha.shape)
+    assert np.isclose(alpha_true, alpha, rtol=1e-3).all()
+    assert np.isclose(mu_true, mu, rtol=1e-3).all()
+    assert np.isclose(Sigma_inv_true, Sigma_inv.numpy(), rtol=1e-3).all()
+    assert np.isclose(Sigma_det_true, Sigma_det.numpy(), rtol=1e-3).all()
+
+    z, log_q_z = mog.forward(params, N=10)
+
+    log_p_z_np = mog.log_prob_np(z, params)
+    assert(np.isclose(log_q_z, log_p_z_np).all())
+
+    log_p_z_torch = mog.log_prob(z, params)
+    assert(np.isclose(log_p_z_torch.numpy(), log_p_z_np, rtol=1e-3).all())
 
     return None
 
@@ -97,11 +145,11 @@ def test_NormFlow():
     assert nf.support_layer == support_layer
 
     nf = de.NormFlow(
-        D, conditioner, arch_type, num_stages, num_layers, 10, tnfb.ToSimplex(D)
+        D, conditioner, arch_type, num_stages, num_layers, 10, bij.ToSimplex(D)
     )
 
     assert nf.num_units == 15
-    assert issubclass(type(nf.support_layer), tnfb.Bijector)
+    assert issubclass(type(nf.support_layer), bij.Bijector)
 
     with raises(TypeError):
         nf = de.NormFlow('foo', False, 'coupling', 1, 2, 20, None)
@@ -143,27 +191,27 @@ def test_NormFlow():
     log_q_z_inv = nf.log_prob(z)
     assert(np.sum(np.square(log_q_z.detach().numpy() - log_q_z_inv.detach().numpy())) < 1e-2)
 
-    nf = de.NormFlow(D, True, 'coupling', 2, 2, 20, tnfb.ToSimplex(D))
-    assert(issubclass(type(nf.bijectors[0]), tnfb.RealNVP))
-    assert(issubclass(type(nf.bijectors[1]), tnfb.BatchNorm))
-    assert(issubclass(type(nf.bijectors[2]), tnfb.RealNVP))
-    assert(issubclass(type(nf.bijectors[3]), tnfb.BatchNorm))
-    assert(issubclass(type(nf.bijectors[4]), tnfb.Affine))
-    assert(issubclass(type(nf.bijectors[5]), tnfb.RealNVP))
-    assert(issubclass(type(nf.bijectors[6]), tnfb.BatchNorm))
-    assert(issubclass(type(nf.bijectors[7]), tnfb.RealNVP))
-    assert(issubclass(type(nf.bijectors[8]), tnfb.BatchNorm))
-    assert(issubclass(type(nf.bijectors[9]), tnfb.Affine))
-    assert(issubclass(type(nf.bijectors[10]), tnfb.ToSimplex))
+    nf = de.NormFlow(D, True, 'coupling', 2, 2, 20, bij.ToSimplex(D))
+    assert(issubclass(type(nf.bijectors[0]), bij.RealNVP))
+    assert(issubclass(type(nf.bijectors[1]), bij.BatchNorm))
+    assert(issubclass(type(nf.bijectors[2]), bij.RealNVP))
+    assert(issubclass(type(nf.bijectors[3]), bij.BatchNorm))
+    assert(issubclass(type(nf.bijectors[4]), bij.Affine))
+    assert(issubclass(type(nf.bijectors[5]), bij.RealNVP))
+    assert(issubclass(type(nf.bijectors[6]), bij.BatchNorm))
+    assert(issubclass(type(nf.bijectors[7]), bij.RealNVP))
+    assert(issubclass(type(nf.bijectors[8]), bij.BatchNorm))
+    assert(issubclass(type(nf.bijectors[9]), bij.Affine))
+    assert(issubclass(type(nf.bijectors[10]), bij.ToSimplex))
 
     nf = de.NormFlow(D, False, 'coupling', 2, 2, 20)
-    assert(issubclass(type(nf.bijectors[0]), tnfb.RealNVP))
+    assert(issubclass(type(nf.bijectors[0]), bij.RealNVP))
     z, log_q_z = nf(N)
     log_q_z_inv = nf.log_prob(z)
     assert(np.sum(np.square(log_q_z.detach().numpy() - log_q_z_inv.detach().numpy())) < 1e-2)
 
     nf = de.NormFlow(D, False, 'AR', num_layers=2, num_units=20)
-    assert(issubclass(type(nf.bijectors[0]), tnfb.MAF))
+    assert(issubclass(type(nf.bijectors[0]), bij.MAF))
     z, log_q_z = nf(N)
     log_q_z_inv = nf.log_prob(z)
 
@@ -173,6 +221,6 @@ def test_NormFlow():
     
 
 if __name__ == "__main__":
-    test_DensityEstimator()
-    test_NormFlow()
+    #test_DensityEstimator()
+    #test_NormFlow()
     test_MoG()
